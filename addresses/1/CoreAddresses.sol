9907ac797d7e97e8bce2c5ba27a3d709edb541cb
kasperpawlowski <kasperpawlowski@gmail.com>
2024-09-17 13:15:49 +0100

    update

---
+++ addresses/1/CoreAddresses.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library CoreAddresses {
+    address public constant balanceTracker = 0x0D52d06ceB8Dcdeeb40Cfd9f17489B350dD7F8a3;
+    address public constant eVaultFactory = 0x29a56a1b8214D9Cf7c5561811750D5cBDb45CC8e;
+    address public constant eVaultFactoryGovernor = 0x799e9b58895d7d10306ca6c4cab51728b142a224;
+    address public constant eVaultImplementation = 0x8Ff1C814719096b61aBf00Bb46EAd0c9A529Dd7D;
+    address public constant evc = 0x0C9a3dd6b8F28529d72d7f9cE918D493519EE383;
+    address public constant permit2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+    address public constant protocolConfig = 0x4cD6BF1D183264c02Be7748Cb5cd3A47d013351b;
+    address public constant sequenceRegistry = 0xEADDD21618ad5Deb412D3fD23580FD461c106B54;
+}
---
+++ addresses/1/LensAddresses.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library LensAddresses {
+    address public constant accountLens = 0x8F59c64fA1Fb2a57e9D084ab3481a13e7db68753;
+    address public constant irmLens = 0x0a6bF6C5BB7301fa6731261c6cFb65df778e7743;
+    address public constant oracleLens = 0xC2Ca699A973A62540C48C749Dc05F890747c5703;
+    address public constant utilsLens = 0xEf3f5dAD6779bd4aee85D8914Af3C23AF5bbd3d1;
+    address public constant vaultLens = 0x8489e3b08B21a611Cd6aA80972E9260F3EC77f71;
+}
