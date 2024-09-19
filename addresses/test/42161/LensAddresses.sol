2185e6720f758d87c7054aaab19be0ddded1d076
kasperpawlowski <kasperpawlowski@gmail.com>
2024-09-19 14:07:26 +0100

    update account lens address on test arbitrum

--- addresses/test/42161/LensAddresses.sol
+++ addresses/test/42161/LensAddresses.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 library LensAddresses {
-    address public constant accountLens = 0xF3415E835c59199AF95423f11dC07aCcD1d40D8F;
+    address public constant accountLens = 0x6924abbb12c1185B1969F8719eDC3642a29e2550;
     address public constant irmLens = 0x5ECeA7412A265B322Ff8D322F274e7c4691a131c;
     address public constant oracleLens = 0x9384dBAB6087706347A822f62B7A31c203806c0b;
     address public constant utilsLens = 0x23378a420Acc6fD3aa17027b723056e1B28a740B;
