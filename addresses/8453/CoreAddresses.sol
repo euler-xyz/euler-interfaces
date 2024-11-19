991c4465ac791477a209220dbfbf860df7b32eae
kasperpawlowski <kasperpawlowski@gmail.com>
2024-11-19 18:40:43 +0900

    commit base ABIs, sync addresses

---
+++ addresses/8453/CoreAddresses.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library CoreAddresses {
+    address public constant balanceTracker = 0x029fDEe85BEdB0553D6fdc538546586641DD7438;
+    address public constant eVaultFactory = 0x7F321498A801A191a93C840750ed637149dDf8D0;
+    address public constant eVaultFactoryGovernor = 0x605Aaf2CAD625dd5F855Bb055625cc1926f10A56;
+    address public constant eVaultImplementation = 0x30a9A9654804F1e5b3291a86E83EdeD7cF281618;
+    address public constant evc = 0x5301c7dD20bD945D2013b48ed0DEE3A284ca8989;
+    address public constant permit2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+    address public constant protocolConfig = 0x1D4b9e6ACACdc82Dd9E903C3F4431558Af32C4A9;
+    address public constant sequenceRegistry = 0xfE9011FD097cd35866b9e4740BBC88B4ef26E3ba;
+}
