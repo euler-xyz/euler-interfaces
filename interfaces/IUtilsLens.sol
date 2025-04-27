9364a0266f07fbe8346e4b991e671b365899a382
kasperpawlowski <kasperpawlowski@gmail.com>
2025-04-26 20:48:45 -0700

    chore: update abis/interfaces

--- interfaces/IUtilsLens.sol
+++ interfaces/IUtilsLens.sol
@@ -15,6 +15,21 @@ interface IUtilsLens {
         uint256 amountOutAsk;
     }
 
+    struct VaultInfoERC4626 {
+        uint256 timestamp;
+        address vault;
+        string vaultName;
+        string vaultSymbol;
+        uint256 vaultDecimals;
+        address asset;
+        string assetName;
+        string assetSymbol;
+        uint256 assetDecimals;
+        uint256 totalShares;
+        uint256 totalAssets;
+        bool isEVault;
+    }
+
     function TTL_ERROR() external view returns (int256);
     function TTL_INFINITY() external view returns (int256);
     function TTL_LIQUIDATION() external view returns (int256);
@@ -29,11 +44,14 @@ interface IUtilsLens {
         external
         pure
         returns (uint256 borrowAPY, uint256 supplyAPY);
+    function eVaultFactory() external view returns (address);
+    function getAPYs(address vault) external view returns (uint256 borrowAPY, uint256 supplyAPY);
     function getAssetPriceInfo(address asset, address unitOfAccount) external view returns (AssetPriceInfo memory);
     function getControllerAssetPriceInfo(address controller, address asset)
         external
         view
         returns (AssetPriceInfo memory);
+    function getVaultInfoERC4626(address vault) external view returns (VaultInfoERC4626 memory);
     function oracleLens() external view returns (address);
     function tokenAllowances(address spender, address account, address[] memory tokens)
         external
