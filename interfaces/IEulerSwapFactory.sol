dfee79815a2093edef24e5a12b8441359bd2d117
kasperpawlowski <kasperpawlowski@gmail.com>
2025-10-31 17:01:11 +0100

    feat: EulerSwap V2 deployment

--- interfaces/IEulerSwapFactory.sol
+++ interfaces/IEulerSwapFactory.sol
@@ -2,24 +2,34 @@
 pragma solidity ^0.8.0;
 
 library IEulerSwap {
-    struct InitialState {
-        uint112 currReserve0;
-        uint112 currReserve1;
-    }
-
-    struct Params {
-        address vault0;
-        address vault1;
-        address eulerAccount;
+    struct DynamicParams {
         uint112 equilibriumReserve0;
         uint112 equilibriumReserve1;
-        uint256 priceX;
-        uint256 priceY;
-        uint256 concentrationX;
-        uint256 concentrationY;
-        uint256 fee;
-        uint256 protocolFee;
-        address protocolFeeRecipient;
+        uint112 minReserve0;
+        uint112 minReserve1;
+        uint80 priceX;
+        uint80 priceY;
+        uint64 concentrationX;
+        uint64 concentrationY;
+        uint64 fee0;
+        uint64 fee1;
+        uint40 expiration;
+        uint8 swapHookedOperations;
+        address swapHook;
+    }
+
+    struct InitialState {
+        uint112 reserve0;
+        uint112 reserve1;
+    }
+
+    struct StaticParams {
+        address supplyVault0;
+        address supplyVault1;
+        address borrowVault0;
+        address borrowVault1;
+        address eulerAccount;
+        address feeRecipient;
     }
 }
 
@@ -27,51 +37,27 @@ interface IEulerSwapFactory {
     error ControllerDisabled();
     error EVC_InvalidAddress();
     error E_DeploymentFailed();
-    error InvalidFee();
-    error InvalidProtocolFee();
-    error InvalidQuery();
-    error InvalidVaultImplementation();
     error NotAuthorized();
-    error OldOperatorStillInstalled();
     error OperatorNotInstalled();
-    error RecipientSetAlready();
-    error SliceOutOfBounds();
     error Unauthorized();
 
-    event OwnershipTransferred(address indexed user, address indexed newOwner);
-    event PoolConfig(address indexed pool, IEulerSwap.Params params, IEulerSwap.InitialState initialState);
-    event PoolDeployed(address indexed asset0, address indexed asset1, address indexed eulerAccount, address pool);
-    event PoolUninstalled(address indexed asset0, address indexed asset1, address indexed eulerAccount, address pool);
-    event ProtocolFeeRecipientSet(address protocolFeeRecipient);
-    event ProtocolFeeSet(uint256 protocolFee);
+    event PoolDeployed(
+        address indexed asset0,
+        address indexed asset1,
+        address indexed eulerAccount,
+        address pool,
+        IEulerSwap.StaticParams sParams
+    );
 
     function EVC() external view returns (address);
-    function MAX_PROTOCOL_FEE() external view returns (uint256);
-    function MIN_PROTOCOL_FEE() external view returns (uint256);
-    function computePoolAddress(IEulerSwap.Params memory poolParams, bytes32 salt) external view returns (address);
-    function deployPool(IEulerSwap.Params memory params, IEulerSwap.InitialState memory initialState, bytes32 salt)
-        external
-        returns (address);
-    function deploymentTimestamp() external view returns (uint256);
-    function enableProtocolFee() external;
+    function computePoolAddress(IEulerSwap.StaticParams memory sParams, bytes32 salt) external view returns (address);
+    function creationCode(IEulerSwap.StaticParams memory sParams) external view returns (bytes memory);
+    function deployPool(
+        IEulerSwap.StaticParams memory sParams,
+        IEulerSwap.DynamicParams memory dParams,
+        IEulerSwap.InitialState memory initialState,
+        bytes32 salt
+    ) external returns (address);
+    function deployedPools(address pool) external view returns (bool);
     function eulerSwapImpl() external view returns (address);
-    function evkFactory() external view returns (address);
-    function owner() external view returns (address);
-    function poolByEulerAccount(address eulerAccount) external view returns (address);
-    function pools() external view returns (address[] memory);
-    function poolsByPair(address asset0, address asset1) external view returns (address[] memory);
-    function poolsByPairLength(address asset0, address asset1) external view returns (uint256);
-    function poolsByPairSlice(address asset0, address asset1, uint256 start, uint256 end)
-        external
-        view
-        returns (address[] memory);
-    function poolsLength() external view returns (uint256);
-    function poolsSlice(uint256 start, uint256 end) external view returns (address[] memory);
-    function protocolFee() external view returns (uint256);
-    function protocolFeeRecipient() external view returns (address);
-    function recipientSetter() external view returns (address);
-    function setProtocolFee(uint256 newFee) external;
-    function setProtocolFeeRecipient(address newRecipient) external;
-    function transferOwnership(address newOwner) external;
-    function uninstallPool() external;
 }
